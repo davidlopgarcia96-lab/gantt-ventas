9b19574aa456c0fc6d207ed002ee7c53eacb5ede
davidlopgarcia96-lab <davidlopgarcia96@gmail.com>
2026-07-16 15:27:12 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Reemplaza estos valores con los que ya copiaste desde Firebase.
 export const firebaseConfig = {
-  apiKey: "REEMPLAZA_API_KEY",
-  authDomain: "REEMPLAZA_PROJECT_ID.firebaseapp.com",
-  projectId: "REEMPLAZA_PROJECT_ID",
-  storageBucket: "REEMPLAZA_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "REEMPLAZA_MESSAGING_SENDER_ID",
-  appId: "REEMPLAZA_APP_ID"
+ apiKey: "AIzaSyCcoiFoTWuqqHEN-OQlMKbTpOrexdLUFN4",
+  authDomain: "diagrama-gant-ventas-tec.firebaseapp.com",
+  projectId: "diagrama-gant-ventas-tec",
+  storageBucket: "diagrama-gant-ventas-tec.firebasestorage.app",
+  messagingSenderId: "470873033103",
+  appId: "1:470873033103:web:43ab4d4452a4013ef363fa"
 };
