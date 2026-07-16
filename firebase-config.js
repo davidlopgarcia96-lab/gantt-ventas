41f7cf219bb233eab3dec157874d75fcd51edae9
davidlopgarcia96-lab <davidlopgarcia96@gmail.com>
2026-07-16 13:58:45 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Reemplaza estos valores con los que ya copiaste desde Firebase.
+export const firebaseConfig = {
+  apiKey: "REEMPLAZA_API_KEY",
+  authDomain: "REEMPLAZA_PROJECT_ID.firebaseapp.com",
+  projectId: "REEMPLAZA_PROJECT_ID",
+  storageBucket: "REEMPLAZA_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "REEMPLAZA_MESSAGING_SENDER_ID",
+  appId: "REEMPLAZA_APP_ID"
+};
